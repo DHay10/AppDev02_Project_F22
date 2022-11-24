0189d3f8a0d8c9c2bf2ff0f83c8e4900459f5ebd
ashmyytaa <ashmi@live.ca>
2022-11-24 03:37:17 -0500

    ooooooooooooooo

--- EventOrganiser/app/src/main/java/com/example/eventorganiser/Attendees.java
+++ EventOrganiser/app/src/main/java/com/example/eventorganiser/Attendees.java
@@ -1,7 +1,32 @@
 package com.example.eventorganiser;
 
-public class Attendees {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Attendees extends AppCompatActivity {
+
+    Button back3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendees);
+
+        back3 = findViewById(R.id.back3);
+
+        back3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Attendees.this,EventOptions.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 
 
