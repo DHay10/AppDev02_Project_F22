1dae2071ebee0a02395ebdbe698ee61ecd318965
ashmyytaa <ashmi@live.ca>
2022-11-25 00:43:01 -0500

    ssss

--- EventOrganiser/app/src/main/java/com/example/eventorganiser/EventListActivity.java
+++ EventOrganiser/app/src/main/java/com/example/eventorganiser/EventListActivity.java
@@ -2,13 +2,32 @@ package com.example.eventorganiser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EventListActivity extends AppCompatActivity {
+
+    Button backB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+
+        backB = findViewById(R.id.backB);
+
+        backB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventListActivity.this,DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
