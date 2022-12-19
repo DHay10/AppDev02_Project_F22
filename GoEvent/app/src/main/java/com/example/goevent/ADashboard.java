ebe3f463782172c84294f539ce49eabab9c3ec96
ashmyytaa <ashmi@live.ca>
2022-12-19 00:11:31 -0500

    Dashboard Activity
    dashboard + added event list activity exclusive to the admin

--- GoEvent/app/src/main/java/com/example/goevent/ADashboard.java
+++ GoEvent/app/src/main/java/com/example/goevent/ADashboard.java
@@ -2,13 +2,59 @@ package com.example.goevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ADashboard extends AppCompatActivity {
 
+    Button adminEventList, adminEventCreate, logout;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adashboard);
+
+
+        //Association
+        adminEventList = findViewById(R.id.eventsB);
+        adminEventCreate = findViewById(R.id.createEventsB);
+        logout = findViewById(R.id.logoutB);
+
+
+
+        //Events Button
+        adminEventList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AEventList.class);
+                startActivity(i);
+            }
+        });
+
+        //Event Create Button
+       adminEventCreate.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(getApplicationContext(), EventCreate.class);
+               startActivity(i);
+           }
+       });
+
+       //Logout Button
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ALogin.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
